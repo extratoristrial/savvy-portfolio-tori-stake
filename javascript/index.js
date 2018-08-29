8977d9d5b54591b3ff1c2ba9d60e0efa79c068e6
Tori Stake <v.m.stake@gmail.com>
2018-08-29 18:56:06 -0500

    adjusted greeting javascript

--- javascript/index.js
+++ javascript/index.js
@@ -1,23 +1,5 @@
-var name = prompt('Hey! What\'s your name?');
 
-var checkName = function() {
-    if (name === '') {
-        name = prompt('What is your name, really?');
+var name = prompt("Hi there! What\'s your name?");
+var output = document.querySelector('#greeting');
 
-        checkName(); 
-    }
-}
-
-// will repeat until there is a name 
-
-checkName() ;
-
-alert('Hello there, ' + name + '!');
-
-console.log(name + ' is exploring the page.');
-
-console.log('See anything you like, ' + name + '?');
-
-console.log('Hello Dave.... ');
-
-alert('Are you feeling it now, Mr. Crabs?');
+output.innerHTML = '<p>Thanks for visiting, ' + name + '.</p>';
