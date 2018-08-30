c2832351e05c6ae338946b5a9fd93290d6f68ff7
Tori Stake <v.m.stake@gmail.com>
2018-08-30 18:36:32 -0500

    changed to relative file paths

--- components/Navigation.js
+++ components/Navigation.js
@@ -1,9 +1,9 @@
 export default `
 <div id="navigation">
 <ul>
-        <li><a href="./index.html" alt="Home">Home</a></li>
-        <li><a href="./blog/index.html" alt="Blog">Blog</a></li>
-        <li><a href="./projects/index.html" alt="Projects" id="list">
+        <li><a href="index.html" alt="Home">Home</a></li>
+        <li><a href="blog/index.html" alt="Blog">Blog</a></li>
+        <li><a href="projects/index.html" alt="Projects" id="list">
             Projects</a>
             <ul>
                 <li>drop 1</li>
@@ -11,7 +11,7 @@ export default `
                 <li>drop 3</li>
             </ul>
         </li>
-        <li><a href="./contact/index.html">Contact</a></li>
+        <li><a href="contact/index.html">Contact</a></li>
     </ul>
 </div>
 `;
